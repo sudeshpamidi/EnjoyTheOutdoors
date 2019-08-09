be2b0aa246fed352b2b0b4d33eeedc0d767d825f
Sudesh Pamidi <sudesh.pamidi@gmail.com>
2019-08-09 14:37:11 -0400

    Refactored the code

--- scripts/parks.js
+++ scripts/parks.js
@@ -3,89 +3,97 @@
 // Author : Sudesh Pamidi
 */
 "use strict"
+
 $(document).ready(function() {
-    let parkTypes = [
-        "National Park",
-        "National Monument",
-        "Recreation Area",
-        "Scenic Trail",
-        "Battlefield",
-        "Historic",
-        "Memorial",
-        "Preserve",
-        "Island",
-        "River",
-        "Seashore",
-        "Trail",
-        "Parkway"
-    ];
 
-    let locations = [
-        "Alabama",
-        "Alaska",
-        "American Samoa",
-        "Arizona",
-        "Arkansas",
-        "California",
-        "Colorado",
-        "Connecticut",
-        "Delaware",
-        "DC",
-        "Florida",
-        "Georgia",
-        "Guam",
-        "Hawaii",
-        "Idaho",
-        "Illinois",
-        "Indiana",
-        "Iowa",
-        "Kansas",
-        "Kentucky",
-        "Louisiana",
-        "Maine",
-        "Maryland",
-        "Massachusetts",
-        "Michigan",
-        "Minnesota",
-        "Mississippi",
-        "Missouri",
-        "Montana",
-        "Nebraska",
-        "Nevada",
-        "New Hampshire",
-        "New Jersey",
-        "New Mexico",
-        "New York",
-        "North Carolina",
-        "North Dakota",
-        "Ohio",
-        "Oklahoma",
-        "Oregon",
-        "Pennsylvania",
-        "Puerto Rico",
-        "Rhode Island",
-        "South Carolina",
-        "South Dakota",
-        "Tennessee",
-        "Texas",
-        "Utah",
-        "Vermont",
-        "Virgin Islands",
-        "Virginia",
-        "Washington",
-        "West Virginia",
-        "Wisconsin",
-        "Wyoming"
-    ];
-
-
+    let parks;
     const parkTypeDropdown = document.getElementById("parkType");
     const locationDropdown = document.getElementById("location");
+    const tbody = document.getElementById("tbody");
+    const thead = document.getElementById("thead");
 
     fillDropDown(parkTypeDropdown, parkTypes);
     fillDropDown(locationDropdown, locations);
 
+    /* begining  of getting JSON file */
+    $.getJSON("/data/nationalparks.json", function(data) {
+        parks = data.parks
 
+        locationDropdown.onchange = refreshTbodyByLocation;
+        parkTypeDropdown.onchange = refreshTbody;
+
+        /** refresh the tbody with data
+         * no parameters         
+         */
+        function refreshTbody() {
+            tbody.innerHTML = "";
+            thead.innerHTML = "";
+            let items = getParksInfoByType(parkTypeDropdown.value);
+            addToThead(thead); // need refactor
+            addToTbody(tbody, items)
+        }
+
+        /** refresh the tbody with data
+         * no parameters         
+         */
+        function refreshTbodyByLocation() {
+            tbody.innerHTML = "";
+            thead.innerHTML = "";
+            let items = getParksInfoByLocation(locationDropdown.value);
+            addToThead(thead); // need refactor
+            addToTbody(tbody, items)
+        }
+
+        /** to add the rows and columms of tbody with data
+         * @param parkType(text) Mountain Name
+         * return the mountains element
+         */
+        function getParksInfoByType(parkType) {
+            let parksInfo = parks.filter(o => o.LocationName.includes(parkType));
+            return parksInfo;
+        };
+
+        /** to add the rows and columms of tbody with data
+         * @param parkType(text) Mountain Name
+         * return the mountains element
+         */
+        function getParksInfoByLocation(location) {
+            let parksInfo = parks.filter(o => o.State == location);
+            return parksInfo;
+        }
+    });
+    /* end of getting JSON file */
+
+    /** to add the rows and columms of tbody with data
+     * @param tbody table body element
+     * @param data  array of elements
+     */
+    function addToTbody(tbody, data) {
+        data.forEach(function(e) {
+            let tr = tbody.insertRow("tr")
+            let i = 0;
+            parktheads.forEach(function(x) {
+                let cell = tr.insertCell(i);
+                let innerHtml = e[x];
+                if (x == "Visit")
+                    innerHtml = (e[x].trim() == "undefined" ? "" : "<a href='" + e[x] + "' target='_blank'>" + e[x] + "</a>")
+
+                cell.innerHTML = innerHtml;
+                i++;
+            });
+        })
+    };
+    /** to add the rows and columms of tbody with data
+     * @param thead table body element     
+     */
+    function addToThead(thead) {
+        let tr = thead.insertRow(0)
+        parktheads.forEach(function(e) {
+            let th = document.createElement("th");
+            th.innerHTML = e.toUpperCase();
+            tr.appendChild(th);
+        });
+    };
 
     //This is to fill the dropDown with the data in array of elements.
     //adding the option dynamically
@@ -96,5 +104,4 @@ $(document).ready(function() {
             nextPos++;
         });
     }
-
 });
